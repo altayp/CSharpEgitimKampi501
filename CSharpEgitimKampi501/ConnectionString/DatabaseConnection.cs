e6ae1e65ef9957b750f19f4f4df11df8c7f65818
Altay Pala <altaypala@gmail.com>
2025-01-17 15:49:44 +0300

    ders 23

---
+++ CSharpEgitimKampi501/ConnectionString/DatabaseConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi501.ConnectionString
+{
+	public static class DatabaseConnection
+	{
+		public static void ConnectToDatabase()
+		{
+			SqlConnection connection = new SqlConnection("Server=ALTAY\\SQLEXPRESS; initial Catalog=EgitimKampi501Db;integrated security=true");
+		}
+	}
+}
--- CSharpEgitimKampi501/Form1.cs
+++ CSharpEgitimKampi501/Form1.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using CSharpEgitimKampi501.Dtos;
+using Dapper;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,9 +20,62 @@ namespace CSharpEgitimKampi501
 			InitializeComponent();
 		}
 
-		private void btnList_Click(object sender, EventArgs e)
-		{
+		SqlConnection connection = new SqlConnection("Server=ALTAY\\SQLEXPRESS; initial Catalog=EgitimKampi501Db;integrated security=true");
 
+		private async void btnList_Click(object sender, EventArgs e)
+		{
+			string query = "select * from TblProduct";
+			var values = await connection.QueryAsync<ResultProductDto>(query);
+			dataGridView1.DataSource = values;
+		}
+
+		private async void btnAdd_Click(object sender, EventArgs e)
+		{
+			string query = "insert into TblProduct (ProductName, ProductStock, ProductPrice, ProductCategory) values (@productName, @productStock, @productPrice, @productCategory)";
+			var parameters = new DynamicParameters();
+			parameters.Add("@productName", txtProductName.Text);
+			parameters.Add("@productStock", txtProductStock.Text);
+			parameters.Add("@productPrice", txtProductPrice.Text);
+			parameters.Add("@productCategory", txtProductCategory.Text);
+			await connection.ExecuteAsync(query, parameters);
+			MessageBox.Show("Ekleme İşlemi Başarılı");
+		}
+
+		private async void btnDelete_Click(object sender, EventArgs e)
+		{
+			string query = "Delete From TblProduct where ProductId = @productId";
+			var parameters = new DynamicParameters();
+			parameters.Add("@productId", txtProductId.Text);
+			await connection.ExecuteAsync(query, parameters);
+			MessageBox.Show("Silme İşlemi Başarılı");
+		}
+
+		private async void btnUpdate_Click(object sender, EventArgs e)
+		{
+			string query = "Update TblProduct set ProductName = @productName, ProductStock = @productStock, ProductPrice = @productPrice, ProductCategory = @productCategory where ProductId = @productId";
+			var parameters = new DynamicParameters();
+			parameters.Add("@productName", txtProductName.Text);
+			parameters.Add("@productStock", txtProductStock.Text);
+			parameters.Add("@productPrice", txtProductPrice.Text);
+			parameters.Add("@productCategory", txtProductCategory.Text);
+			parameters.Add("@productId", txtProductId.Text);
+			await connection.ExecuteAsync(query, parameters);
+			MessageBox.Show("Güncelleme İşslemi Başarılı");
+		}
+
+		private async void Form1_Load(object sender, EventArgs e)
+		{
+			string query1 = "Select count(*) from TblProduct";
+			var productTotalCount = await connection.QueryFirstOrDefaultAsync<int>(query1);
+			lblTotalProductCount.Text = productTotalCount.ToString();
+
+			string query2 = "select ProductName from TblProduct where ProductPrice = (select max(ProductPrice) from TblProduct)";
+			var maximumPriceProductName = await connection.QueryFirstOrDefaultAsync<string>(query2);
+			lblMaxPriceProductName.Text = maximumPriceProductName.ToString();
+
+			string query3 = "select count(distinct(ProductCategory)) from TblProduct";
+			var distinctProductCount = await connection.QueryFirstOrDefaultAsync<int>(query3);
+			lblDistinctCategoryCount.Text = distinctProductCount.ToString();
 		}
 	}
 }
